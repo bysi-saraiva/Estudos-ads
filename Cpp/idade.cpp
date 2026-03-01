ad402f34ec70f1233da1a46126f79f5186728805
Sibelly <sibellysaraivaalves@gmail.com>
2026-03-01 12:29:56 -0300

    Organizando pastas do projeto

---
+++ Cpp/idade.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+   int main () {
+       int idade;
+
+       cout<<"Qual é a sua idade?";
+       cin>> idade;
+        
+      if (idade >= 18) {
+        cout<<"Você é maior de idade"<< endl;
+      } else { 
+        cout<<"Você é menor de idade"<< endl; 
+    }
+
+      return 0;
+    }
