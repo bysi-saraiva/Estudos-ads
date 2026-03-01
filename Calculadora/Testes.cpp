c197f66a5bc802ebba2733a89d206c2348f997dd
Sibelly <sibellysaraivaalves@gmail.com>
2026-02-28 22:08:58 -0300

    repete

--- Calculadora/Testes.cpp
+++ Calculadora/Testes.cpp
@@ -9,6 +9,7 @@ int main() {
 
     return 0;
 }
+
 #include <iostream>
 using namespace std;
 
@@ -25,6 +26,28 @@ int main () {
     }
 
     return 0;
+}
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+      string nome
+      int idade;
+cout<< "Qual o seu nome?"<<endl;
+cin>> nome;
+cout<<"Qual a sua idade?"<<endl;
+cin>> idade;
+if (idade >= 18) {
+cout<<nome<<", você é maior de idade"<<endl;
+} 
+else { cout<<nome<<", você é menor de idade"<<endl;
+}
+
+return 0;
+
 }
 
 #include <iostream>
