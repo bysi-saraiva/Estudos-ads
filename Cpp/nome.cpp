7364bfdd1a871e52b4db15513813edbb398de473
Sibelly <sibellysaraivaalves@gmail.com>
2026-03-07 23:35:11 -0300

    nome e idade

--- Cpp/nome.cpp
+++ Cpp/nome.cpp
@@ -34,3 +34,43 @@ else if (nota >= 5 && nota < 7) {
 else {
     cout << "Reprovação";
 }
+#include <iostream>
+using namespace std;
+     int main () {
+      int nota;
+      cout<<"Digite sua nota:";
+      cin>>nota
+      
+if (nota >= 7) {
+    cout << "Aprovado";
+} 
+else if (nota >= 5 && nota < 7) {
+    cout << "Recuperação";
+} 
+else {
+    cout << "Reprovação";
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string nome;
+    int idade;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Digite sua idade: ";
+    cin >> idade;
+
+    if (idade >= 18) {
+        cout << nome << " voce eh maior de idade";
+    }
+    else {
+        cout << nome << " voce eh menor de idade";
+    }
+
+    return 0;
+}
