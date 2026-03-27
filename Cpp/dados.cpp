48953b95eea25eaf508d46ba5b0f8be13a26944c
Sibelly <sibellysaraivaalves@gmail.com>
2026-03-26 23:22:59 -0300

    entrada de dados do sistema financeiro

---
+++ Cpp/dados.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    float renda;
+    float gastos;
+    float percentual;
+    float sobra;   
+
+cout<<"Digite seu nome:";
+cin>>nome;
+
+cout<<"Digite sua renda:";
+cin>>renda;
+
+cout<<"Digite seus gastos:";         
+cin>>gastos;
+                                                                                                                
+
+    return 0;
+}
