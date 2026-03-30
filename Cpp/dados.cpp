260688b05da4a17d85775a693bf969f9ad1fd25c
Sibelly <sibellysaraivaalves@gmail.com>
2026-03-29 22:45:09 -0300

    implementa analise financeira com calculo e classificacao

--- Cpp/dados.cpp
+++ Cpp/dados.cpp
@@ -8,15 +8,37 @@ int main() {
     float percentual;
     float sobra;   
 
-cout<<"Digite seu nome:";
-cin>>nome;
+    cout << "Digite seu nome: ";
+    cin >> nome;
 
-cout<<"Digite sua renda:";
-cin>>renda;
+    cout << "Digite sua renda: ";
+    cin >> renda;
 
-cout<<"Digite seus gastos:";         
-cin>>gastos;
-                                                                                                                
+    if (renda <= 0) {
+        cout << "Renda invalida";
+        return 0;
+    }
+
+    cout << "Digite seus gastos: ";
+    cin >> gastos;
+
+    percentual = (gastos / renda) * 100;
+    sobra = renda - gastos;
+
+    cout << "Percentual: " << percentual << "%" << endl;
+    cout << "Sobra: " << sobra << endl;
+
+    cout << nome << ", sua situacao financeira: ";
+
+    if (percentual <= 60) {
+        cout << "Saudavel";
+    }
+    else if (percentual <= 90) {
+        cout << "Alerta";
+    }
+    else {
+        cout << "Risco";
+    }
 
     return 0;
 }
