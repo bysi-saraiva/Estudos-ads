44f5ab3e68b3097f7cbb19bcb2336df9ae531917
Sibelly <sibellysaraivaalves@gmail.com>
2026-04-27 21:41:21 -0300

    Exemplo de desconto

--- Cpp/dados.cpp
+++ Cpp/dados.cpp
@@ -42,3 +42,34 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    float valorCompra;
+    float valorFinal;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Digite o valor da compra: ";
+    cin >> valorCompra;
+
+    if (valorCompra <= 100) {
+        valorFinal = valorCompra;
+    }
+    else if (valorCompra <= 300) {
+        valorFinal = valorCompra - (valorCompra * 0.10);
+    }
+    else {
+        valorFinal = valorCompra - (valorCompra * 0.20);
+    }
+
+    cout << "Cliente: " << nome << endl;
+    cout << "Valor original: " << valorCompra << endl;
+    cout << "Valor final: " << valorFinal << endl;
+
+    return 0;
+}
